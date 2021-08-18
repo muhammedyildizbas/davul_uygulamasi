70eb6a9bbe1b2a5972d76ca46531ada48b249cc5
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-08-18 14:53:52 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -7,28 +8,103 @@ void main(){
 }
 
 class DrumMachine extends StatelessWidget {
-  final player = AudioCache();
+
 
   @override
   Widget build(BuildContext context) {
   return MaterialApp(
     home:Scaffold(
-      body: SafeArea(
-        child:FlatButton(
-          onPressed: (){
-          player.play('bongo.wav');
-          },
-          child: Container(
-          color: Colors.lightBlue,
-          ),
-        ),
-
-
-
-      ),
+      backgroundColor: Colors.black38,
+      body:DrumPage()
     )
   );
 
   }
 
 }
+class DrumPage extends StatelessWidget {
+  final player = AudioCache();
+  void sesiCal(String ses){
+    player.play('$ses.wav');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child:
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children:<Widget> [
+            Expanded(
+              child: Row(
+                children:<Widget> [
+                  Expanded(
+                    child: buildDrumPad('bongo', Colors.blue),
+                  ),
+                Expanded(child:
+                  buildDrumPad('clap3',Colors.lime),
+                  ),
+
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children:<Widget> [
+                  Expanded(
+                    child: buildDrumPad('crash',Colors.grey),
+                  ),
+                  Expanded(child:
+                  buildDrumPad('ridebel',Colors.orange),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children:<Widget> [
+                  Expanded(
+                    child: buildDrumPad('clap1', Colors.lime),
+                  ),
+                  Expanded(child:
+                  buildDrumPad('bongo',Colors.pink),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children:<Widget> [
+                  Expanded(
+                    child: buildDrumPad('clap2', Colors.purple),
+                  ),
+                  Expanded(child:
+                  buildDrumPad('bongo',Colors.greenAccent),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+
+
+    );
+  }
+
+  FlatButton buildDrumPad(String ses , Color renk) {
+    return FlatButton(
+                    padding:EdgeInsets.all(8),
+                    onPressed: (){
+                      sesiCal(ses);
+
+                    },
+                    child: Container(
+                      color: renk,
+                    ),
+                  );
+  }
+}
+
